4943d2c3f4260db11d5eee432fe3df67d2652156
Gustaf Erickson <gerickson808@gmail.com>
2016-04-26 13:03:34 -0400

    Fixed project page for mobile

--- browser/js/common/factories/ProjectFactory.js
+++ browser/js/common/factories/ProjectFactory.js
@@ -2,10 +2,17 @@ app.factory('ProjectFactory', function($http, $state, $rootScope){
 
 	var factory = {};
 	factory.currentProject = null;
-
 	factory.getProjects = function(){
 		return $http.get('/api/projects')
-		.then(response=>response.data);
+		.then(response=>response.data)
+		.then(projects=>{
+			this.projects = projects;
+			return projects;
+		});
+	};
+
+	factory.getProject = function(name){
+		return $.grep(this.projects, e => e.name===name)[0];
 	};
 
 	factory.getCurrentProject = function(){
@@ -14,7 +21,6 @@ app.factory('ProjectFactory', function($http, $state, $rootScope){
 
 	factory.showProject = function(project){
 		this.currentProject = project;
-		console.log($state.current.name);
 		if($state.current.name != "projects.project") $state.go('projects.project',{project:project});
 		else $rootScope.$broadcast('newProject');
 	};
--- browser/js/projects/small-project-box.js
+++ browser/js/projects/small-project-box.js
@@ -1,18 +1,16 @@
-app.directive('projectBox', function(ProjectFactory){
+app.directive('smallProjectBox', function(ProjectFactory){
 	return {
 		restrict:'E',
 		templateUrl:'js/projects/project-box.html',
-		scope:{},
+		scope:{
+			name:'='
+		},
 		link: function(scope){
-			scope.project = ProjectFactory.getCurrentProject();
+			scope.project = ProjectFactory.getProject(scope.name);
 			scope.currentImage = scope.project.imageLinks[0];
 			scope.showImage = function(image){
 				scope.currentImage = image;
 			};
-			scope.$on('newProject', function(){
-        scope.project = ProjectFactory.getCurrentProject();
-        scope.currentImage = scope.project.imageLinks[0];
-    });
 			scope.hideLink = function(){
 				return scope.project.deployedLink === null;
 			};
