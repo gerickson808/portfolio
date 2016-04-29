2aaa354d91952421e96fcabc5530c5e845d9d152
Gustaf Erickson <gerickson808@gmail.com>
2016-04-29 11:59:11 -0400

    Mobile version, image sizes

--- seed.js
+++ seed.js
@@ -31,32 +31,32 @@ var seedProjects = function () {
             description:'Fireframe is a web-based wireframing tool with real-time collaboration and easy-to-use version control.',
             repoLink:'https://github.com/gerickson808/fireframe',
             deployedLink:'http://firefra.me',
-            logoUrl: 'img/FireFrame.png',
-            imageLinks:['img/fireframe-editor.png', 'img/fireframe-projects.png', 'img/version-control.png']
+            logoUrl: 'img/FireFrame.jpg',
+            imageLinks:['img/fireframe-editor.jpg', 'img/fireframe-projects.jpg', 'img/version-control.jpg']
         },
         {
             name:'Help Jimmy',
             description:'Jimmy is a little red ball in a dangerous world, learning to navigate with the help of a Q-learning neural network.',
             repoLink: 'https://github.com/gerickson808/learningblob',
             deployedLink: 'http://rawgit.com/gerickson808/learningblob/presentation/index.html',
-            logoUrl: 'img/jimmy.png',
-            imageLinks: ['img/jimmy-roll.png','img/jimmy-jump.png','img/jimmy-control.png']
+            logoUrl: 'img/jimmy.jpg',
+            imageLinks: ['img/jimmy-roll.jpg','img/jimmy-jump.jpg','img/jimmy-control.jpg']
         },
         {
             name:'STOR',
             description:'A questionnaire-centric, recommendation-based marketplace for vendors to sell anything and everything.',
             repoLink: 'https://github.com/gholevas/stackstore',
             deployedLink: 'http://stackstore.herokuapp.com',
-            logoUrl: 'img/stor.png',
-            imageLinks:['img/stor-frontpage.png', 'img/stor-stors.png','img/stor-question1.png','img/stor-question2.png','img/stor-product.png']
+            logoUrl: 'img/stor.jpg',
+            imageLinks:['img/stor-frontpage.jpg', 'img/stor-stors.jpg','img/stor-question1.jpg','img/stor-question2.jpg','img/stor-product.jpg']
         },
         {
             name:'Bingle',
             description: 'A chrome extension combining the beauty of the Bing homepage with the usefulness of Google search results. Don\'t tell them.',
             repoLink: 'https://github.com/gerickson808/bingle',
             deployedLink: null,
-            logoUrl: 'img/bingle-logo.png',
-            imageLinks: ['img/bingle-homepage.png','img/bingle-results.png']
+            logoUrl: 'img/bingle-logo.jpg',
+            imageLinks: ['img/bingle-homepage.jpg','img/bingle-results.jpg']
         }
 
     ];
